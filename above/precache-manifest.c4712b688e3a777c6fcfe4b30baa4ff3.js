cc1720bf98ac580f17333ac8982cda4e6753e3e5
Ahmad Fozael Amiri <89026669+FOZAEL@users.noreply.github.com>
2022-07-18 12:44:12 +0430

    above

---
+++ above/precache-manifest.c4712b688e3a777c6fcfe4b30baa4ff3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161a4a53d17dc1030924fa1f6a329ab9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "895ad72f8ad787b3dc92",
+    "url": "/static/css/main.e4f782f6.chunk.css"
+  },
+  {
+    "revision": "759fa1c341cf35c63a5a",
+    "url": "/static/js/2.d350ccde.chunk.js"
+  },
+  {
+    "revision": "895ad72f8ad787b3dc92",
+    "url": "/static/js/main.9308619e.chunk.js"
+  },
+  {
+    "revision": "80b789232f75104194ee",
+    "url": "/static/js/runtime-main.e2fb13b6.js"
+  }
+]);
