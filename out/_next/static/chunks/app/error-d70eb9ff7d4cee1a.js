4fe0c855edc863d9a415ffb07bb79193aabd59b6
cris <89026669+FOZAEL@users.noreply.github.com>
2023-09-22 16:52:45 +0430

    test nextjs export

---
+++ out/_next/static/chunks/app/error-d70eb9ff7d4cee1a.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[601],{9382:function(e,r,n){Promise.resolve().then(n.bind(n,6352))},6352:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return s}});var t=n(7437),o=n(2265);function s(e){let{error:r,reset:n}=e;return(0,o.useEffect)(()=>{console.error(r)},[r]),(0,t.jsxs)("div",{children:[(0,t.jsx)("h2",{children:"یک مشکلی پیش امده"}),(0,t.jsx)("p",{children:"لطفا صفحه را مجددا بارگذاری کنید"}),(0,t.jsx)("button",{onClick:()=>n(),children:"مجددا تلاش کنید"})]})}},622:function(e,r,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=n(2265),o=Symbol.for("react.element"),s=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),u=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function f(e,r,n){var t,f={},i=null,l=null;for(t in void 0!==n&&(i=""+n),void 0!==r.key&&(i=""+r.key),void 0!==r.ref&&(l=r.ref),r)s.call(r,t)&&!c.hasOwnProperty(t)&&(f[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===f[t]&&(f[t]=r[t]);return{$$typeof:o,type:e,key:i,ref:l,props:f,_owner:u.current}}r.jsx=f,r.jsxs=f},7437:function(e,r,n){"use strict";e.exports=n(622)}},function(e){e.O(0,[971,596,744],function(){return e(e.s=9382)}),_N_E=e.O()}]);
